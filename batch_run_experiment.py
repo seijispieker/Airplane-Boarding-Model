fd0ad2b833cf553a0a1fef4fc281a93a992c4563
Seiji Spieker <seiji.spieker@gmail.com>
2025-01-28 22:14:00 +0100

    new batch_run_experiments

--- batch_run_experiment.py
+++ batch_run_experiment.py
@@ -10,23 +10,33 @@ parameters = {
     "steps_per_second": 2,
     "aisle_speed": 0.8,
     "number_of_passengers": range(round(0.71 * 174), round(0.81 * 174) + 1),
-    "seat_assignment_method": [
-        "random",
-        "segmented_random_3",
-        "segmented_random_4",
-        "outside_in",
-        "steffen_perfect"
-    ],
     "conformance": range(0, 101, 5)
 }
+
+
+seat_assignment_methods = [
+    # "back_to_front",
+    # "random",
+    "segmented_random_3",
+    "segmented_random_4",
+    "outside_in",
+    "steffen_perfect"
+]
 
 
 def main():
     if not os.path.exists("results"):
         os.makedirs("results")
     
+    for method in seat_assignment_methods:
+        seat_assignment_method(method)
+
+
+def seat_assignment_method(method):
     if not os.path.exists("results/experiment"):
         os.makedirs("results/experiment")
+        
+    parameters["seat_assignment_method"] = method
         
     results = batch_run(
         model_cls=BoardingModel,
@@ -39,22 +49,18 @@ def main():
     )
     
     results_df = pd.DataFrame(results)
-    results_df = results_df.drop(columns=["iteration"], inplace=False)
-    
-    results_df = results_df.drop_duplicates(
-        subset=["RunId"],
-        inplace=False
-    )
-    results_df = results_df.drop(
+    results_df.drop(columns=["iteration"], inplace=True)
+    results_df.drop_duplicates(subset=["RunId"], inplace=True)
+    results_df.drop(
         columns=[
             "AgentID",
             "Seat shuffle time (s)",
             "Seat shuffle type (A/B/C/D)",
         ],
-        inplace=False
+        inplace=True
     )
     
-    results_df.to_csv("results/experiment/results.csv")
+    results_df.to_csv(f"results/experiment/results_{method}.csv")
 
 
 if __name__ == '__main__':
